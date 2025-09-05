44dc299e0afb974954eeadd2aa5c8ed46ce3539b
Tainá Bandeira <tainabandeira45@gmail.com>
2025-09-04 22:07:55 -0300

    Update inciante.c

--- inciante.c
+++ inciante.c
@@ -31,6 +31,32 @@ int main(){
     }
     
 
+    //Movimentação do cavalo em L
+    int Cavalo = 1;
+
+    while (Cavalo--)  /*é o operador de pós-decremento.
+
+Ele testa o valor atual, e depois diminui em 1.
+
+No início:
+
+Cavalo = 1
+
+while (Cavalo--) → verifica 1 (verdadeiro).
+Depois da verificação, Cavalo vira 0.
+
+Na próxima vez que o while tentar rodar:
+
+Cavalo = 0
+
+while (0--) → verifica 0 (falso), e para o loop.*/
+    {
+        for (int i = 0; i < 2; i++) {
+            printf("Cavalo - Cima\n"); 
+        }
+        printf("Cavalo - Direita\n");   
+    }
+
     return 0;
     
 }
